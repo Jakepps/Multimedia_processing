ceab84627869b4e0f26e1fa7c209b06b9ccf4b92
Jakepps <96076243+Jakepps@users.noreply.github.com>
2023-09-09 12:08:57 +0300

    task 7

---
+++ LR1/task7.py
@@ -0,0 +1,19 @@
+import cv2
+
+def readIPWriteTOFile():
+    video = cv2.VideoCapture(0)
+    ok, img = video.read()
+    w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    video_writer = cv2.VideoWriter("output_webcam.mp4", fourcc, 25, (w, h))
+    while (True):
+        ok, img = video.read()
+        cv2.imshow('img', img)
+        video_writer.write(img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    video.release()
+    cv2.destroyAllWindows()
+
+readIPWriteTOFile()
