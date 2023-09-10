ddc612f59007ce3e73c60ededd27393f496d451c
Jakepps <96076243+Jakepps@users.noreply.github.com>
2023-09-10 14:07:49 +0300

    task 9

---
+++ LR1/task9.py
@@ -0,0 +1,18 @@
+import requests
+import cv2
+import numpy as np
+import imutils
+
+url = "http://192.168.0.136:8080/shot.jpg"
+
+while True:
+    img_resp = requests.get(url)
+    img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
+    img = cv2.imdecode(img_arr, -1)
+    img = imutils.resize(img, width=1000, height=1800)
+    cv2.imshow("Android_cam", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
