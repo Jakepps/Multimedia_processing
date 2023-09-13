541476b6e3dd7b3e30d428dd9438f73b4d039f1e
Jakepps <96076243+Jakepps@users.noreply.github.com>
2023-09-13 10:07:03 +0300

    LR1-task1

---
+++ LR2/task1.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    cv2.imshow('hsv_frame', hsv)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
