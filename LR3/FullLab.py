45f43e3f64e3b18fb7a85a6c7e3f4070f2801cff
Jakepps <nagalevsiy@mail.ru>
2023-09-24 17:28:40 +0300

    фиксы и комментарии

--- LR3/FullLab.py
+++ LR3/FullLab.py
@@ -3,31 +3,28 @@ import numpy as np
 
 
 def BlurFuss():
-    # 1 - строим матрицу, задаём значения и картинку
+    # 1 - cтроим матрицу, задаём значения и картинку
     img = cv2.imread("test.jpg", cv2.IMREAD_GRAYSCALE)
 
+    # Cтандартное отклонение и размер ядра фильтра
     standard_deviation = 100
     kernel_size = 5
+
     imgBlur1 = GaussianBlur(img, kernel_size, standard_deviation)
-    cv2.imshow(str(kernel_size)+'x'+str(kernel_size) +
-               ' and ' + str(standard_deviation), imgBlur1)
+    cv2.imshow(str(kernel_size) + 'x' + str(kernel_size) + ' and deviation ' + str(standard_deviation), imgBlur1)
 
     # 4 - другие параметры
     standard_deviation = 50
     kernel_size = 11
     imgBlur2 = GaussianBlur(img, kernel_size, standard_deviation)
-    cv2.imshow(str(kernel_size)+'x'+str(kernel_size) +
-               ' and ' + str(standard_deviation), imgBlur2)
+    cv2.imshow(str(kernel_size)+'x'+str(kernel_size) + ' and deviation ' + str(standard_deviation), imgBlur2)
 
-    # 5 - блюр от opencv
     imgBlurOpenCV = cv2.GaussianBlur(img, (kernel_size, kernel_size), standard_deviation)
 
     cv2.imshow('img', img)
-    cv2.imshow('OpenCV', imgBlurOpenCV)
+    cv2.imshow('OpenCV_blur', imgBlurOpenCV)
     cv2.waitKey(0)
 
-
-                      #размер ядра фильтра, стандартное отклонение для гаус.функции
 def GaussianBlur(img, kernel_size, standard_deviation):
     kernel = np.ones((kernel_size, kernel_size))
     a = b = (kernel_size + 1) // 2
@@ -39,7 +36,7 @@ def GaussianBlur(img, kernel_size, standard_deviation):
 
     print(kernel)
 
-    # 2 - Нормализуем (для сохранения яркости изображения)
+    # 2 - нормализуем для сохранения яркости изображения (сумма всех элементов должна быть = 1)
     sum = 0
     for i in range(kernel_size):
         for j in range(kernel_size):
@@ -47,31 +44,30 @@ def GaussianBlur(img, kernel_size, standard_deviation):
     for i in range(kernel_size):
         for j in range(kernel_size):
             kernel[i, j] /= sum
+
     print(kernel)
 
-    # 3 - используем матрицу
-    # Проходимся по внутренним пикселям
-    # (каждый пиксель изображения умножается на соответствующее значение в ядре, а затем суммируется)
+    # 3 - проходим через внутренние пиксели изображения и выполняем операцию свертки между изображением и ядром.
+    # каждый пиксель изображения умножается на соответствующее значение в ядре, а затем суммируется
     imgBlur = img.copy()
     x_start = kernel_size // 2
     y_start = kernel_size // 2
-    for i in range(x_start, imgBlur.shape[0]-x_start):
-        for j in range(y_start, imgBlur.shape[1]-y_start):
-
+    for i in range(x_start, imgBlur.shape[0] - x_start):
+        for j in range(y_start, imgBlur.shape[1] - y_start):
             # операция свёртки
             val = 0
-            for k in range(-(kernel_size//2), kernel_size//2+1):
-                for l in range(-(kernel_size//2), kernel_size//2+1):
-                    val += img[i + k, j + l] * kernel[k + (kernel_size//2), l + (kernel_size//2)]
+            for k in range(-(kernel_size // 2), kernel_size // 2 + 1):
+                for l in range(-(kernel_size // 2), kernel_size // 2 + 1):
+                    val += img[i + k, j + l] * kernel[k + (kernel_size // 2), l + (kernel_size // 2)]
             imgBlur[i, j] = val
 
     return imgBlur
 
-
+        #коорд.пикселя, станд.откл, координаты центра ядра
 def gauss(x, y, omega, a, b):
-    omegaIn2 = 2 * omega ** 2
-    m1 = 1 / (np.pi * omegaIn2)
-    m2 = np.exp(-((x-a) ** 2 + (y-b) ** 2) / omegaIn2)
+    omega2 = 2 * omega ** 2
+    m1 = 1 / (np.pi * omega2)
+    m2 = np.exp(-((x-a) ** 2 + (y-b) ** 2) / omega2)
     return m1 * m2
 
 BlurFuss()
