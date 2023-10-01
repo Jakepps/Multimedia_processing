e58e8db331c8741b3379ebb422cfe076f396117d
Jakepps <nagalevsiy@mail.ru>
2023-10-01 15:25:59 +0300

    комментарии

--- LR4/Lab4.py
+++ LR4/Lab4.py
@@ -7,6 +7,7 @@ def svertka(img, kernel):
     x_start = kernel_size // 2
     y_start = kernel_size // 2
     matr = np.zeros(img.shape)
+
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
             matr[i][j] = img[i][j]
@@ -18,8 +19,7 @@ def svertka(img, kernel):
             val = 0
             for k in range(-(kernel_size//2), kernel_size//2+1):
                 for l in range(-(kernel_size//2), kernel_size//2+1):
-                    val += img[i + k][j + l] * kernel[k +
-                                                      (kernel_size//2)][l + (kernel_size//2)]
+                    val += img[i + k][j + l] * kernel[k + (kernel_size//2)][l + (kernel_size//2)]
             matr[i][j] = val
 
     return matr
@@ -62,13 +62,13 @@ def get_angle_number(x, y):
 
 i = 0
 
-def task(path, standard_deviation, kernel_size, bound_path):
+def main(path, standard_deviation, kernel_size, bound_path):
     global i
     i += 1
+
     # 1
     img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    imgBlurByCV2 = cv2.GaussianBlur(
-        img, (kernel_size, kernel_size), standard_deviation)
+    imgBlurByCV2 = cv2.GaussianBlur(img, (kernel_size, kernel_size), standard_deviation)
 
     cv2.imshow(path, imgBlurByCV2)
 
@@ -80,6 +80,7 @@ def task(path, standard_deviation, kernel_size, bound_path):
     img_Gx = svertka(img, Gx)
     img_Gy = svertka(img, Gy)
 
+    #общий градиент
     matr_gradient = np.zeros(img.shape)
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
@@ -87,9 +88,9 @@ def task(path, standard_deviation, kernel_size, bound_path):
 
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
-            matr_gradient[i][j] = np.sqrt(
-                img_Gx[i][j] ** 2 + img_Gy[i][j] ** 2)
+            matr_gradient[i][j] = np.sqrt(img_Gx[i][j] ** 2 + img_Gy[i][j] ** 2)
 
+    #углы градиента для каждого пикселя
     img_angles = img.copy()
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
@@ -100,24 +101,25 @@ def task(path, standard_deviation, kernel_size, bound_path):
     max_gradient = np.max(matr_gradient)
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
-            img_gradient_to_print[i][j] = (
-                float(matr_gradient[i][j])/max_gradient)*255
-    cv2.imshow('img_gradient_to_print ' + str(i), img_gradient_to_print)
+            #нормализация значения градиента относительно макс значения и масшабирования в диапозоне от 0 до 255
+            img_gradient_to_print[i][j] = (float(matr_gradient[i][j]) / max_gradient) * 255
+    cv2.imshow('img gradient to print ' + str(i), img_gradient_to_print)
 
     img_angles_to_print = img.copy()
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
-            img_angles_to_print[i][j] = img_angles[i][j]/7*255
-    cv2.imshow('img_angles_to_print ' + str(i), img_angles_to_print)
+            img_angles_to_print[i][j] = img_angles[i][j] / 7 * 255
+    cv2.imshow('img angles to print ' + str(i), img_angles_to_print)
 
     # 3
-    img_border_not_filtered = img.copy()
+    img_border_no_filter = img.copy()
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
             angle = img_angles[i][j]
             gradient = matr_gradient[i][j]
+            #находится ли текущий пиксель на грани изображения(на границе матрицы)
             if (i == 0 or i == img.shape[0] - 1 or j == 0 or j == img.shape[1] - 1):
-                img_border_not_filtered[i][j] = 0
+                img_border_no_filter[i][j] = 0
             else:
                 x_shift = 0
                 y_shift = 0
@@ -135,34 +137,39 @@ def task(path, standard_deviation, kernel_size, bound_path):
                 else:
                     y_shift = 1
 
-                is_max = gradient >= matr_gradient[i+y_shift][j +
-                                                              x_shift] and gradient >= matr_gradient[i-y_shift][j-x_shift]
-                img_border_not_filtered[i][j] = 255 if is_max else 0
-    cv2.imshow('img_border_not_filtered ' + str(i), img_border_not_filtered)
+                #явл ли текущий пиксель локльным максимумом градиента в направлении угла
+                is_max = gradient >= matr_gradient[i + y_shift][j + x_shift] and gradient >= matr_gradient[i-y_shift][j-x_shift]
+                img_border_no_filter[i][j] = 255 if is_max else 0
+
+    cv2.imshow('img border no filter ' + str(i), img_border_no_filter)
 
     # 4
-    lower_bound = max_gradient/bound_path
-    upper_bound = max_gradient - max_gradient/bound_path
-    img_border_filtered = np.zeros(img.shape)
+    #для понимания когда лок максимум градиента будут считаться границами
+    lower_bound = max_gradient / bound_path
+    upper_bound = max_gradient - max_gradient / bound_path
+    img_border_filter = np.zeros(img.shape)
+
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
             gradient = matr_gradient[i][j]
-            if (img_border_not_filtered[i][j] == 255):
+            #является лок максимумом
+            if (img_border_no_filter[i][j] == 255):
                 if (gradient >= lower_bound and gradient <= upper_bound):
                     flag = False
+                    #проверим соседние пиксели текусщего пикселя
                     for k in range(-1, 2):
                         for l in range(-1, 2):
                             if (flag):
                                 break
-                            if (img_border_not_filtered[i+k][j+l] == 255 and matr_gradient[i+k][j+l] >= lower_bound):
+                            if (img_border_no_filter[i+k][j+l] == 255 and matr_gradient[i+k][j+l] >= lower_bound):
                                 flag = True
                                 break
                     if (flag):
-                        img_border_filtered[i][j] = 255
+                        img_border_filter[i][j] = 255
                 elif (gradient > upper_bound):
-                    img_border_filtered[i][j] = 255
+                    img_border_filter[i][j] = 255
 
-    cv2.imshow('img_border_filtered ' + str(i), img_border_filtered)
+    cv2.imshow('img border filter ' + str(i), img_border_filter)
 
     while True:
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -170,6 +177,6 @@ def task(path, standard_deviation, kernel_size, bound_path):
 
 
 # 5
-task('test.jpg', 10, 3, 15)
-task('test.jpg', 10, 3, 6)
-task('test.jpg', 100, 11, 15)
+main('test.jpg', 10, 3, 15)
+main('test.jpg', 20, 10, 6)
+main('test.jpg', 100, 16, 15)
