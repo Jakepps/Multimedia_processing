46d11caffecf11313a466b52eeb05af3534e1e82
Jakepps <nagalevsiy@mail.ru>
2023-11-28 19:55:02 +0300

    Отчет + qa

--- LR7/TesseractOCR_test.py
+++ LR7/TesseractOCR_test.py
@@ -1,7 +1,6 @@
 import pytesseract
 import cv2
 
-# Укажите путь к исполняемому файлу TesseractOCR
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 img = cv2.imread('dataset/6.jpg')
 text = pytesseract.image_to_string(img,lang='rus+eng')
